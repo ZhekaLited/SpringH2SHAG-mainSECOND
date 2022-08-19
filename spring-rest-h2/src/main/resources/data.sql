a53753a4752558175cfd073b7da0f3e04f438351
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-08-19 17:52:55 +0300

    Update data.sql

---
+++ spring-rest-h2/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+ insert into PUBLIC.Car ( car_Name , car_Color, car_Model) values ('BMW','BLACK','E34');
+ insert into Car ( car_Name , car_Color, car_Model) values ('MERCEDES','WHITE','W140');
+ insert into Car ( car_Name , car_Color, car_Model) values ('LADA','BLACK','PRIORA');
+ insert into Car ( car_Name , car_Color, car_Model) values ('GEELY','GREEN','453');
+ insert into Car ( car_Name , car_Color, car_Model) values ('TOYOTA','BLACK','3.5');
+ insert into Car ( car_Name , car_Color, car_Model) values ('AUDI','RED','100');
+
+
+ insert into PUBLIC.Country (car_Id,country_Name,city_Name) values (1,'GERMANY','Toluca de Lerdo');
+ insert into Country (car_Id,country_Name,city_Name) values (2,'GERMANY','Stuttgart');
+ insert into Country (car_Id,country_Name,city_Name) values (3,'RUSSIA','Tolyatti');
+ insert into Country (car_Id,country_Name,city_Name) values (4,'CHINE','Shanghai');
+ insert into Country (car_Id,country_Name,city_Name) values (5,'JAPAN','TOKIO');
+ insert into Country (car_Id,country_Name,city_Name) values (6,'GERMANY','wolfsburg');
+
+
+ insert into PUBLIC.Price (country_Id,price_Car,credit_Price) values (1,1000,2000);
+ insert into PUBLIC.Price (country_Id,price_Car,credit_Price) values (2,4000,5000);
+ insert into PUBLIC.Price (country_Id,price_Car,credit_Price) values (3,1500,2500);
+ insert into PUBLIC.Price (country_Id,price_Car,credit_Price) values (4,5000,5500);
+ insert into PUBLIC.Price (country_Id,price_Car,credit_Price) values (5,10000,12000);
+ insert into PUBLIC.Price (country_Id,price_Car,credit_Price) values (6,3000,3850);
