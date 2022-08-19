15db0956e9ba2665b3d16ce38711e0a7a26e53a6
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-08-19 17:55:21 +0300

    Update Country.java

--- spring-rest-h2/src/main/java/org/itstep/helloworldspring/Model/Country.java
+++ spring-rest-h2/src/main/java/org/itstep/helloworldspring/Model/Country.java
@@ -1,2 +1,37 @@
-package org.itstep.helloworldspring.Model;public class Country {
+package org.itstep.helloworldspring.Model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@AllArgsConstructor
+@ToString(exclude = "car_id")
+@Table(name = "country")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_Id", nullable = false)
+    private Long country_Id;
+    @Column(name = "country_Name", nullable = false)
+    private String countryName;
+    @Column(name = "city_Name", nullable = false)
+    private String cityName;
+    @OneToOne
+    @JoinColumn(name = "car_id", unique = true, nullable = false)
+    @JsonBackReference
+    private Car car;
+    @OneToOne(mappedBy = "country")
+    private Price price;
+
 }
