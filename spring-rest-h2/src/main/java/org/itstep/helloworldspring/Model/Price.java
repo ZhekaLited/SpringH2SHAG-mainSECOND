b6874b8e150b86c88fd3238f9cdcf76eb4ea3e79
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-08-19 17:56:03 +0300

    Update Price.java

--- spring-rest-h2/src/main/java/org/itstep/helloworldspring/Model/Price.java
+++ spring-rest-h2/src/main/java/org/itstep/helloworldspring/Model/Price.java
@@ -1,2 +1,38 @@
-package org.itstep.helloworldspring.Model;public class Price {
+package org.itstep.helloworldspring.Model;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@AllArgsConstructor
+@ToString(exclude = "country_id")
+@Table(name = "price")
+
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "price_Id", nullable = false)
+    private Long price_Id;
+    @Column(name = "price_Car", nullable = false)
+    private int priceCar;
+    @Column(name = "credit_Price", nullable = false)
+    private int credit_Price;
+    @OneToOne
+    @JoinColumn(name = "country_Id", unique = true, nullable = false)
+    @JsonBackReference
+    private Country country;
+
+
 }
